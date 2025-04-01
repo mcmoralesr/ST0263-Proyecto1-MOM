1d4b32b0530374468612f55d6e6a9d986c103225
manuelArango <manuelargo2004@gmail.com>
2025-03-31 21:00:11 -0500

    Se arregla un import

--- revistas-app/src/App.jsx
+++ revistas-app/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
-import Magazines from './components/magazines';
+import Magazines from './components/Magazines';
 function App() {
     return (
         <Router>
